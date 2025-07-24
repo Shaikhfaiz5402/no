7cf2c23ff6d84ce3e0c5c75bcd7383a5f4eccada
faisal shaikh <shaikhfaisalafzal21@gmail.com>
2025-07-24 18:01:35 +0530

    Update search placeholder text

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
             ))}
 
             <div className='hidden lg:flex items-center text-sm gap-2 border border-borderColor px-3 rounded-full max-w-56'>
-                <input type="text" className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" placeholder="Search cars"/>
+                <input type="text" className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" placeholder="Search bikes"/>
                 <img src={assets.search_icon} alt="search" />
             </div>
 
